6ff91dd576fc67a9afb1711138a2001bf28f13a9
vietdangsbit <128115784+vietdangsbit@users.noreply.github.com>
2023-07-20 11:03:13 +0700

    Project.4

--- scrip.js
+++
@@ -1,6 +0,0 @@
-const optionMenu = document.querySelector(".form"),
-        selectBtn = optionMenu.querySelector(".icon-muiten"),
-        options = optionMenu.querySelectorAll(".option"),
-        sBtn_text = optionMenu.querySelector(".chu");
-
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
